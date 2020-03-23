30d3952b4ed287558aa1a40730260832293db2ab
Jair Escamilla <hernandez_yo14@outlook.es>
2020-03-22 19:58:38 -0600

    Insertando comentarios

--- menu.c
+++ menu.c
@@ -8,7 +8,7 @@
 void menu(){
     system("clear");
     puts("\t\t\tMENÚ\n");
-    puts("\t1.- Altas (Clientes/Asesores/Productos/Categorias/Departamentos.)");
+    puts("\t1.- Altas (Clientes/Asesores/Productos/Categorias/Departamentos/Comentarios.)");
     puts("\t2.- Desplegar datos (Clientes/Asesores/Productos/Categorias/Departamentos).");
     puts("\t3.- Actualizar datos (Clientes/Asesores/Productos/Categorias/Departamentos).");
     puts("\t4.- Eliminar (Clientes/Asesores/Productos/Categorias/Departamentos).");
@@ -24,13 +24,15 @@ void menuAltas(Conexion con){
         {"nombre del asesor", "apellido materno", "apellido paterno"},
         {"nombre del producto", "descripcion", "precio", "stock", "costo de envio"},
         {"nombre de la categoria", "descripcion de la categoria", "id del departamento al que pertenece"},
-        {"nombre del departamento"}
+        {"nombre del departamento"},
+        {"username", "idProducto", "comentario"}
     };
     char camposDB[5][7][150] = {
         {"nombre", "apPaterno", "apMaterno"},
         {"nombreProducto", "descripcion", "precio", "stock", "costoEnvio"},
         {"nombreCategoria", "descripcionCategoria", "idDepartamento"},
-        {"nombreDepartamento"}
+        {"nombreDepartamento"},
+        {"pr1_clientes_username", "pr1_productos_idProducto", "comentario"}
     };
     system("clear");
     puts("\t\t\tMENÚ ALTAS\n");
@@ -39,7 +41,8 @@ void menuAltas(Conexion con){
     puts("\t3.- Productos.");
     puts("\t4.- Categorias.");
     puts("\t5.- Departamentos.");
-    puts("\t6.- Volver.");
+    puts("\t6.- Comentarios.");
+    puts("\t7.- Volver.");
     printf("\nIngresar opcion-> ");
     scanf(" %d", &opcionSubMenu);
     if (opcionSubMenu == 1)
@@ -52,6 +55,8 @@ void menuAltas(Conexion con){
         insertarDatos(campos[2], 3, "pr1_categorias_productos", camposDB[2], con);
     if (opcionSubMenu == 5)
         insertarDatos(campos[3], 1, "pr1_departamentos", camposDB[3], con);
+    if (opcionSubMenu == 6)
+        insertarDatos(campos[4], 3, "pr1_comentarios", camposDB[4], con);
 }
 
 void menuActualizaciones(Conexion con){
